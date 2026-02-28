707b9894e5d833cd58b9bef95c91abe07f3f844f
guisilvas <guilhermesoaressilvadev@gmail.com>
2026-02-28 02:24:33 -0300

    Some tests in cpp

--- computer-science/2nd-period/pdsII/class/01/main.cpp
+++ computer-science/2nd-period/pdsII/class/01/main.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
+#include <string>
+#include <math.h>
+#include <array>
 
 using namespace std;
 
 int magnitude(int x)
 {
-    if x<10:
-    return 
+    string s = to_string(x);
+    return pow(10,s.length() - 1);
+}
+
+int* factor(int x)
+{
+    int* res = new int[1];
+    return res;
 }
 
 int main()
 {
-    cout << "This is a test" << endl;
+    int num = 0;
+    cout << "Enter a value: ";
+    cin >> num;
+    int* ptr = factor(num);
+    cout << "Magnitude of " << num << ": " << magnitude(num) << endl;
+    cout << "Factors of " << num << ": " << ptr[0] << endl;
+    delete[] ptr;
+    return 0;
 }
