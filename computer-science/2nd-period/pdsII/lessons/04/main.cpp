2497543da50d025092247820acf6b47f17818362
guisilvas <guilhermesoaressilvadev@gmail.com>
2026-03-15 21:40:06 -0300

    Lesson 04 some stuffs

--- computer-science/2nd-period/pdsII/lessons/04/main.cpp
+++ computer-science/2nd-period/pdsII/lessons/04/main.cpp
@@ -1,7 +1,26 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int mian() {
-    return 0;
+int main() {
+    int* p = nullptr;
+    int *q = nullptr;
+    int * r = 0;
+    int a = 2;
+    int* s = new int;
+    
+    cout<<"a = "<<a<<endl<<"&a = "<<&a<<endl;
+    
+    p = &a;
+    q = &a;
+    r = &a;
+    *s = a;
+    
+    cout<<endl<<"p = "<<p<<endl<<"&p = "<<&p<<endl<<"*p = "<<*p<<endl;
+    cout<<endl<<"q = "<<q<<endl<<"&q = "<<&q<<endl<<"*q = "<<*q<<endl;
+    cout<<endl<<"r = "<<r<<endl<<"&r = "<<&r<<endl<<"*r = "<<*r<<endl;
+    cout<<endl<<"s = "<<s<<endl<<"&s = "<<&s<<endl<<"*s = "<<*s<<endl;
+    
+    delete s;
+    
+  return 0;
 }
